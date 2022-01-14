7aaa4ed8794a416a6e2524492b91e70347768283
Omar Jiménez <omarjimdiz-coder@github.com>
2022-01-14 13:44:29 -0600

    Custom hook with useState, I created a hook reusable

---
+++ src/hooks/useCounter.js
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+const useCounter = (initialState = 10) => {
+    
+    const [state, setState] = useState(initialState);
+
+    const increment = () => {
+        setState(state + 1);
+    }
+
+    const decrement = () => {
+        setState(state - 1);
+    }
+
+    const reset = () => {
+        setState(initialState);
+    }
+
+    return {
+        state,
+        increment,
+        decrement,
+        reset
+    }
+
+}
+
+export default useCounter
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
 // import HooksApp from './HooksApp';
-import CounterApp from './components/01-useState/CounterApp';
+// import CounterApp from './components/01-useState/CounterApp';
 
 ReactDOM.render(
-    <CounterApp />,
+    <CounterWithCustomHook />,
   document.getElementById('root')
 );
 
