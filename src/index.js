a6cc6e0be765136add34b7faae46348d19750401
Omar Jiménez <omarjimdiz-coder@github.com>
2022-01-18 22:58:47 -0600

    Created useFetch with others hooks like useState and useEffect, this is a mix with some CustomHooks

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
+import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
+// import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
 // import SimpleForm from './components/02-useEffect/SimpleForm';
 // import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
 // import HooksApp from './HooksApp';
 // import CounterApp from './components/01-useState/CounterApp';
 
 ReactDOM.render(
-    <FormWithCustomHook />,
+    <MultipleCustomHooks />,
   document.getElementById('root')
 );
 
